26a95fb72f3244eded5ebda803f9a3ae3eb48579
FreGitHu <freddiejscott@outlook.com>
2026-08-30 09:49:01 +0100

    rebrand to BreadMod

---
+++ src/lib/themes/accent/bread.js
@@ -0,0 +1,24 @@
+const guiColors = {
+    "motion-primary": "#e8ae68",
+    "motion-primary-transparent": "#e8ae68e6",
+    "motion-tertiary": "#d69340",
+
+    "looks-secondary": "#e8ae68",
+    "looks-transparent": "#d69a5159",
+    "looks-light-transparent": "#d69a5126",
+    "looks-secondary-dark": "#dfa45c",
+
+    "extensions-primary": "#f2b161",
+    "extensions-tertiary": "#e88a17",
+    "extensions-transparent": "#e8ae6859",
+    "extensions-light": "#ffddb3",
+
+    "drop-highlight": "#ffddb3",
+};
+
+const blockColors = {
+    checkboxActiveBackground: "#e8ae68",
+    checkboxActiveBorder: "#d69340",
+};
+
+export { guiColors, blockColors };
